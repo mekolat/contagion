a80de405e908ce14114edac8a1bea4cf8c6a77c2
gumi <mekolat@users.noreply.github.com>
2017-02-09 22:16:47 -0500

    improve caching

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,59 @@
-var cacheName = "Contagion1";
-var filesToCache = [
-  "/",
-  "/index.html",
-  "/license.html",
-  "/LICENSE",
-  "/app.min.js",
-  "/default.css",
-  "/normalize.css",
-  "/favicon.ico",
-  "/icons/512.svg",
-  "/icons/256.png",
-  "/icons/128.png",
-  "/icons/64.png",
-  "/icons/48.png",
-  "/icons/32.png",
+var cacheName = "Contagion-static-{{git-rev}}";
+var essentialFiles = [
+    "/",
+    "/app.min.js",
+    "/default.css",
+    "/normalize.css",
+];
+var miscFiles = [
+    "/manifest.json",
+    "/license.html",
+    "/LICENSE",
+    "/favicon.ico",
+    "/icons/512.svg",
+    "/icons/512.png",
+    "/icons/256.png",
+    "/icons/144.png",
+    "/icons/128.png",
+    "/icons/64.png",
+    "/icons/48.png",
+    "/icons/32.png",
 ];
 
 self.addEventListener("install", function(e) {
-  console.log("[ServiceWorker] Install");
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log("[ServiceWorker] Caching app");
-      return cache.addAll(filesToCache);
-    })
-  );
+    console.log("[ServiceWorker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log("[ServiceWorker] Caching app", cacheName);
+            cache.addAll(miscFiles); // cache non-essential files later
+            return cache.addAll(essentialFiles); // cache essential files now
+        })
+    );
 });
 
 self.addEventListener("activate", function(e) {
-  console.log("[ServiceWorker] Activate");
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log("[ServiceWorker] Removing old cache", key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
+    console.log("[ServiceWorker] Activate");
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log("[ServiceWorker] Removing old cache", key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
 });
 
 self.addEventListener("fetch", function(e) {
-  console.log("[Service Worker] Fetch", e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+    console.log("[Service Worker] Fetch", e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request).then(function(response){
+                console.warn("[Service Worker] Not found in cache", e.request.url);
+                return response;
+            });
+        })
+    );
 });
